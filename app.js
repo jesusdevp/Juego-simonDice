147dbbe3b6e72ef8e1e513c0e42a99115abddabd
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-05 11:59:35 -0600

    Se agregaron los estados finales del juego

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const violeta = document.getElementById("violeta");
 const naranja = document.getElementById("naranja");
 const verde = document.getElementById("verde");
 const btnEmpezar = document.getElementById("btnEmpezar");
-const ULTIMO_NIVEL = 15;
+const ULTIMO_NIVEL = 10;
 
 class Juego {
   constructor() {
+    this.inicializar = this.inicializar.bind(this);
     this.inicializar();
     this.generarSecuencia();
     setTimeout(this.siguienteNivel, 500);
@@ -15,7 +16,8 @@ class Juego {
   inicializar() {
     this.siguienteNivel = this.siguienteNivel.bind(this);
     this.elegirColor = this.elegirColor.bind(this);
-    btnEmpezar.classList.add("hide");
+    this.toggleBtnEmpezar();
+
     this.nivel = 1;
     this.colores = {
       celeste,
@@ -23,6 +25,14 @@ class Juego {
       naranja,
       verde
     };
+  }
+
+  toggleBtnEmpezar() {
+    if (btnEmpezar.classList.contains("hide")) {
+      btnEmpezar.classList.remove("hide");
+    } else {
+      btnEmpezar.classList.add("hide");
+    }
   }
 
   generarSecuencia() {
@@ -65,7 +75,7 @@ class Juego {
 
   iluminarSecuencia() {
     for (let i = 0; i < this.nivel; i++) {
-      let color = this.transformarNumeroAColor(this.secuencia[i]);
+      const color = this.transformarNumeroAColor(this.secuencia[i]);
       setTimeout(() => this.iluminarColor(color), 1000 * i);
     }
   }
@@ -97,20 +107,35 @@ class Juego {
     const nombreColor = ev.target.dataset.color;
     const numeroColor = this.transformarColorANumero(nombreColor);
     this.iluminarColor(nombreColor);
-    if (numeroColor === this.secuencia[this.subniel]) {
+    if (numeroColor === this.secuencia[this.subnivel]) {
       this.subnivel++;
       if (this.subnivel === this.nivel) {
         this.nivel++;
         this.eliminarEventosClick();
         if (this.nvel === ULTIMO_NIVEL + 1) {
-          // gano
+          this.ganoElJuego();
         } else {
           setTimeout(this.siguienteNivel, 1500);
         }
       }
     } else {
-      // perdio
+      this.perdioElJuego();
     }
+  }
+
+  ganoElJuego() {
+    swal("Ganaste!", "Felicitaciones, ganaste el juego!", "success").then(
+      this.inicializar.bind(this)
+    );
+  }
+
+  perdioElJuego() {
+    swal("Perdiste", "Lo lamentamos, vuelve a intentarlo :(", "error").then(
+      () => {
+        this.eliminarEventosClick();
+        this.inicializar();
+      }
+    );
   }
 }
 
